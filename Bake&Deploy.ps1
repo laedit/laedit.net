ab55ad17106481ace94e4acd32f024e91502d0b4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-10-26 17:08:58 +0200

    Use creep to deploy files

---
+++ Bake&Deploy.ps1
@@ -0,0 +1,8 @@
+C:\tools\Pretzel\pretzel bake src
+
+if ($lastExitCode -eq 0)
+{
+    Write-Host "Starting deploy"
+    $envConf = "{{`"`"default`"`": {{`"`"connection`"`": `"`"ftp://{0}:{1}@laedit.net`"`"}}}}" -f $env:ftp_user, $env:ftp_password
+    creep -e $envConf -d "{`"`"source`"`": `"`"hash`"`"}" -b src/_site -y
+}
