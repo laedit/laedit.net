5dec935ef7d95c00faa789278a36a0e0e904aa03
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-10-26 18:10:42 +0200

    In the right file this time

--- Bake&Deploy.ps1
+++ Bake&Deploy.ps1
@@ -3,7 +3,6 @@ C:\tools\Pretzel\pretzel bake src
 if ($lastExitCode -eq 0)
 {
     Write-Host "Starting deploy"
-    $env:Path += ";C:\\Python35;C:\\Python35\\Scripts"
     $envConf = "{{`"`"default`"`": {{`"`"connection`"`": `"`"ftp://{0}:{1}@laedit.net`"`"}}}}" -f $env:ftp_user, $env:ftp_password
     creep -e $envConf -d "{`"`"source`"`": `"`"hash`"`"}" -b src/_site -y
 }
