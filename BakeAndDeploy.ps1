d55e7ca3c4c620c59a36c5e82370f438236b803d
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-10-28 08:52:30 +0200

    Testing with an error on creep

--- BakeAndDeploy.ps1
+++ BakeAndDeploy.ps1
@@ -3,7 +3,7 @@ C:\tools\Pretzel\pretzel bake src
 if ($lastExitCode -eq 0)
 {
     Write-Host "Starting deploy"
-    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@laedit.net""}}}}' -f $env:ftp_user, $env:ftp_password
+    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@laedit.net"}}}}' -f $env:ftp_user, $env:ftp_password
     creep -e $envConf -d '{""source"": ""hash""}' -b src/_site -y
 
     if ($lastExitCode -ne 0)
