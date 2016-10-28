17a1d133cfe67bb2457f17ea9d7d22b5a09145f6
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-10-28 08:43:39 +0200

    Trying with double double quotes

--- BakeAndDeploy.ps1
+++ BakeAndDeploy.ps1
@@ -3,6 +3,6 @@ C:\tools\Pretzel\pretzel bake src
 if ($lastExitCode -eq 0)
 {
     Write-Host "Starting deploy"
-    $envConf = '{{"default": {{"connection": "ftp://{0}:{1}@laedit.net"}}}}' -f $env:ftp_user, $env:ftp_password
-    creep -e $envConf -d '{"source": "hash"}' -b src/_site -y
+    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@laedit.net"}}}}' -f $env:ftp_user, $env:ftp_password
+    creep -e $envConf -d '{""source"": ""hash""}' -b src/_site -y
 }
